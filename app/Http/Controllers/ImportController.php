91aa6dc5158c2635bae841811dc89db2763ea5c7
sonia <soniarani486@gmail.com>
2016-10-01 20:12:42 +0530

    focus

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -34,7 +34,7 @@ class ImportController extends Controller
                                        $model->TPPD     = $emapData[0];
                                        $model->ORVT     = $emapData[1];
                                        $model->OFVT     = $emapData[2];
-                                       $VEVT            = $filepart[1];
+                                       $VEVT            = $filepart[4];
                                        if($VEVT == ''){
                                            $VEVT        = 0;
                                        }
